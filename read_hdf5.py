081724689485aa5b50a509f47b594df421e8eb88
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2023-08-22 14:07:54 +1000

    A python reader

---
+++ read_hdf5.py
@@ -0,0 +1,20 @@
+import h5py
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+filename = 'solution_0.h5'
+with h5py.File(filename, 'r') as f:
+    # Access data within the group
+    temperature = f['temperature'][:]
+    timestamps = f['timestamps'][:]
+
+
+plt.close(1)
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
+ax.imshow(temperature[50, :, :],
+          cmap=cm.get_cmap('jet', 10),
+          extent=(0, 2, 0, 1))
+
+ax.invert_yaxis()
+fig.show()
