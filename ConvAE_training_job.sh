5ade157ee16c2ab8ead8d7974f7c30810ba4c776
Xuzeng He <u7189309@anu.edu.au>
2023-08-27 18:35:05 +1000

    Trained ConvAE using Gadi and renamed some files for 1D problem

---
+++ ConvAE_training_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ 
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=40GB
+#PBS -l jobfs=10GB
+#PBS -q gpuvolta
+#PBS -P kr97
+#PBS -l walltime=00:30:00
+#PBS -l storage=scratch/kr97
+#PBS -l wd
+  
+module load pytorch/1.10.0
+
+python3 ConvAE_training.py > /scratch/kr97/$USER/comp4560/$PBS_JOBID.log
